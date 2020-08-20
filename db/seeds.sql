3363612dcd5d21e4e771b3c24c3a5cdab2c4d186
Cameron Graham <cameron.g882@gmail.com>
2020-08-20 10:28:39 -0400

    initial commit with db setup, working on model next

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('Black & Blue');
+INSERT INTO burgers (name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Real Veggie Burger');
+INSERT INTO burgers (name) VALUES ('Fake Meat Burger');
+INSERT INTO burgers (name, devoured) VALUES ('BBQ Bacon Burger', true);
